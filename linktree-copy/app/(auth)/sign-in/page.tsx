bfce78ed724aa8365c64e9071e46d8aef8f52eda
B-Chehin <chehinbautista@gmail.com>
2025-06-04 19:38:10 -0300

    layouts

---
+++ linktree-copy/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+
+export default function SignInPage() {
+  return (
+    <div>
+        <h1>Sign in page</h1>
+    
+        <Button>Sign in</Button>
+    </div>
+  )
+}
---
+++ linktree-copy/app/(auth)/sign-up/paxe.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button"
+
+export default function SingUpPage() {
+  return (
+    <div>
+        <h1>Sign up page</h1>
+
+        <Button>Sign up</Button>
+    </div>
+  )
+}
