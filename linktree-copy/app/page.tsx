a0360a998385c491a3ff6732277de0ae03c27204
B-Chehin <chehinbautista@gmail.com>
2025-06-05 01:01:19 -0300

    Toast

--- linktree-copy/app/page.tsx
+++ linktree-copy/app/page.tsx
@@ -1,7 +1,19 @@
+"use client";
+import { useEffect } from "react";
 import { Button } from "@/components/ui/button";
-
+import { toast } from "sonner";
 
 export default function Home() {
+
+  useEffect(() => {
+    toast("Hola Mundo", {
+      description: "Esta es una notificación de prueba",
+      duration: 5000,
+      position: "top-right",
+    });
+  }
+  , []);
+
   return (
     <div>
 
